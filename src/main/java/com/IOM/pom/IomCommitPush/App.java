07fa4fd0f805d6b05d4c010b580d6f7014486355
Avinash Rana <Avinashrana@INLaptop135-2.local>
2019-02-22 17:33:24 +0530

    First commit

---
+++ src/main/java/com/IOM/pom/IomCommitPush/App.java
@@ -0,0 +1,13 @@
+package com.IOM.pom.IomCommitPush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
